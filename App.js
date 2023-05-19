a14ddc3b92fd179d08f50bbd7630e25d2459d2c3
0xnunana <kudayapaul@gmail.com>
2023-05-19 19:55:59 +0000

    cea with nativewind

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500">Open up App.js to start working on your application</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
