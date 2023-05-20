de218e5439a2267db8ca22c177c5572b96a55013
0xnunana <kudayapaul@gmail.com>
2023-05-20 08:31:33 +0000

    only two paths required, APP and SRC

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}",
-    "./components.{js,jsx,ts,tsx}"
+    //all others should be in a folder
+    "./src/**/*.{js,jsx,ts,tsx}",
+
   ],
   theme: {
     extend: {},
